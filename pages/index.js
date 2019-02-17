9273f8ad951923db94b165a161b4e26bec8a04b1
heff <git@heff.me>
2019-02-17 15:23:40 -0800

    Some basic structure

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,51 @@
-export default class extends React.Component {
+import React from 'react';
+import Link from 'next/link';
+import styled from 'styled-components'
 
-  static async getInitialProps ({ query }) {
-    const { p } = query
-    const stories = await getStories('topstories', { page })
-    return { page, stories }
+const Title = styled.h1`
+  font-size: 50px;
+`;
+
+// nogame | starting | started
+let Content = (props) => {
+  if (props.gameState == 'starting') {
+    return (
+      <p>Enter your name</p>
+    );
+  } else if (props.gameState == 'started') {
+    return (
+      <p>A game has already started. You missed it.</p>
+    );
+  } else {
+    return (
+      <p>There is no game right now.</p>
+    );
   }
+};
 
-  render () {
-    const { page, url, stories } = this.props
-    const offset = (page - 1) * 30
-    return <Page>
-      <Stories page={page} offset={offset} stories={stories} />
-    </Page>
-  }
+const Index = () => (
+  <div>
+    <Title>Welcome to trivia.dev!</Title>
+    <Content />
+  </div>
+);
 
-}
+export default Index;
+
+// export default class extends React.Component {
+//
+//   static async getInitialProps ({ query }) {
+//     const { p } = query
+//     const stories = await getStories('topstories', { page })
+//     return { page, stories }
+//   }
+//
+//   render () {
+//     const { page, url, stories } = this.props
+//     const offset = (page - 1) * 30
+//     return <Page>
+//       <Stories page={page} offset={offset} stories={stories} />
+//     </Page>
+//   }
+//
+// }
