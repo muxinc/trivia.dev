c68320c5ae43cf9c43559070aac9d049ce6f5db0
heff <git@heff.me>
2019-02-18 14:52:00 -0800

    Updated states

--- pages/index.js
+++ pages/index.js
@@ -6,26 +6,65 @@ const Title = styled.h1`
   font-size: 50px;
 `;
 
-// nogame | starting | started
-let Content = props => {
-  if (props.gameState == 'starting') {
-    return <p>Enter your name</p>;
-  } else if (props.gameState == 'started') {
-    return <p>A game already started</p>;
-  } else {
-    return <p>There is no game right now</p>;
-  }
-};
-
 class Index extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { currentUser: undefined };
+    this.state = {
+      currentUser: undefined,
+      currentGameId: undefined,
+    };
   }
-  async componentDidMount() {
+
+  componentDidMount() {
+    this.gameWatcher();
+  }
+
+  async gameWatcher() {
     const { firebase, db } = await initialize();
     this.firebase = firebase;
     this.db = db;
+
+    // Listen for an open game
+    let unsubscribeToGames = this.db
+      .collection('games')
+      .where('state', '==', 'open')
+      .limit(1)
+      .onSnapshot(querySnapshot => {
+        let gameId;
+        querySnapshot.forEach(function(doc) {
+          gameId = doc.id;
+        });
+
+        if (gameId) {
+          console.log('gameId', gameId);
+          unsubscribeToGames();
+          this.setState({
+            currentGameId: gameId,
+          });
+          // Subscribe to game updates
+          this.subscribeToGame();
+        }
+      });
+  }
+
+  subscribeToGame() {
+    console.log('this.state.currentGameId', this.state.currentGameId);
+
+    this.unsubscribeFromGame = this.db
+      .collection('games')
+      .doc(this.state.currentGameId)
+      .onSnapshot(
+        {
+          // Listen for document metadata changes
+          includeMetadataChanges: true,
+        },
+        doc => {
+          console.log(doc.data());
+          this.setState({
+            currentGame: doc.data(),
+          });
+        }
+      );
   }
 
   login = async () => {
@@ -34,52 +73,50 @@ class Index extends React.Component {
     await this.firebase
       .auth()
       .setPersistence(this.firebase.auth.Auth.Persistence.SESSION);
-    //
-    // const session = await this.firebase.auth().signInWithPopup(authProvider);
 
-    const querySnapshot = await this.db
-      .collection('currentGame')
-      .get('players');
+    const session = await this.firebase.auth().signInWithPopup(authProvider);
 
-    console.log(querySnapshot);
-
-    // querySnapshot.forEach(doc => {
-    //   console.log(doc.data());
-    // });
-
-    // this.db
-    //   .collection('currentGame').get()
-    //   .players.add({
-    //     username: session.additionalUserInfo.username,
-    //   })
-    //   .then(function(docRef) {
-    //     console.log('Document written with ID: ', docRef.id);
-    //   })
-    //   .catch(function(error) {
-    //     console.error('Error adding document: ', error);
-    //   });
-
-    // this.setState({
-    //   currentUser: {
-    //     username: session.additionalUserInfo.username,
-    //     email: session.user.email,
-    //     name: session.user.displayName,
-    //   },
-    // });
-    // console.log(session);
+    this.setState({
+      currentUser: {
+        username: session.additionalUserInfo.username,
+        email: session.user.email,
+        name: session.user.displayName,
+      },
+    });
+    console.log(session);
   };
 
   render() {
     return (
       <div>
         <Title>Welcome to trivia.dev!</Title>
+
         {!this.state.currentUser && (
           <button onClick={this.login}>Log in with Github</button>
         )}
+
         {this.state.currentUser && (
           <p>Oh hai, {this.state.currentUser.username}</p>
         )}
-        <Content />
+
+        {this.state.currentUser && !this.state.currentGameId && (
+          <p>
+            There's no game starting right now. Try back when a show is
+            scheduled.
+          </p>
+        )}
+
+        {this.state.currentUser &&
+          this.state.currentGameId &&
+          this.state.currentGame.state == 'open' && (
+            <p>You're in the game! The game will start soon!</p>
+          )}
+
+        {this.state.currentUser &&
+          this.state.currentGameId &&
+          this.state.currentGame.state == 'started' && (
+            <p>You're in the game!</p>
+          )}
       </div>
     );
   }
