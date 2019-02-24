a49c355d15d752627b3c72334ecd26c44a8415a6
Steve Heffernan <git@heff.me>
2019-02-24 14:12:26 -0800

    Just making a PR to make reviewing easier (#1)
    * progress?
    
    * Updated states
    
    * Started adding answers
    
    * Updated with PR suggestions.

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,67 @@ const Title = styled.h1`
   font-size: 50px;
 `;
 
-// nogame | starting | started
-let Content = props => {
-  if (props.gameState == 'starting') {
-    return <p>Enter your name</p>;
-  } else if (props.gameState == 'started') {
-    return <p>A game already started. You missed it. :(</p>;
-  } else {
-    return <p>There's no game right now.</p>;
-  }
-};
-
 class Index extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { currentUser: undefined };
+    this.state = {
+      currentUser: undefined,
+      currentGameId: undefined,
+    };
   }
-  async componentDidMount() {
-    const { firebase } = await initialize();
+
+  componentDidMount() {
+    this.gameWatcher();
+  }
+
+  async gameWatcher() {
+    const { firebase, db } = await initialize();
     this.firebase = firebase;
+    this.db = db;
+
+    // Listen for an open game
+    let unsubscribeToGames = this.db
+      .collection('games')
+      .where('state', '==', 'open')
+      .limit(1)
+      .onSnapshot(querySnapshot => {
+        console.log('game watcher');
+
+        let gameId;
+        querySnapshot.forEach(function(doc) {
+          gameId = doc.id;
+        });
+
+        if (gameId) {
+          console.log('gameId', gameId);
+          unsubscribeToGames();
+          this.setState({
+            currentGameId: gameId,
+          });
+          // Subscribe to game updates
+          this.subscribeToGame();
+        }
+      });
+  }
+
+  subscribeToGame() {
+    console.log('this.state.currentGameId', this.state.currentGameId);
+
+    this.unsubscribeFromGame = this.db
+      .collection('games')
+      .doc(this.state.currentGameId)
+      .onSnapshot(
+        {
+          // Listen for document metadata changes
+          includeMetadataChanges: true,
+        },
+        doc => {
+          console.log(doc.data());
+          this.setState({
+            currentGame: doc.data(),
+          });
+        }
+      );
   }
 
   login = async () => {
@@ -47,16 +89,42 @@ class Index extends React.Component {
   };
 
   render() {
+    const { currentUser, currentGameId, currentGame } = this.state;
+    const currentQuestion = currentGame && currentGame.currentQuestion;
+
     return (
       <div>
         <Title>Welcome to trivia.dev!</Title>
-        {!this.state.currentUser && (
+
+        {!currentUser && (
           <button onClick={this.login}>Log in with Github</button>
         )}
-        {this.state.currentUser && (
-          <p>Oh hai, {this.state.currentUser.username}</p>
+
+        {currentUser && <p>Oh hai, {currentUser.username}</p>}
+
+        {currentUser && !currentGameId && (
+          <p>
+            There's no game starting right now. Try back when a show is
+            scheduled.
+          </p>
         )}
-        <Content />
+
+        {currentUser && currentGameId && currentGame.state == 'open' && (
+          <p>You're in the game! The game will start soon!</p>
+        )}
+
+        {currentUser && currentGameId && currentGame.state == 'started' && (
+          <p>You're in the game!</p>
+        )}
+
+        {currentQuestion && (
+          <div>
+            <p>{currentQuestion.question}</p>
+            {currentQuestion.answers.map((answer, i) => (
+              <p key={i}>{answer}</p>
+            ))}
+          </div>
+        )}
       </div>
     );
   }
