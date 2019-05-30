1e365678157caf328cae7e6876308407e428c28a
Matthew McClure <m@mmcc.io>
2019-05-30 09:32:32 -0700

    Added github back as login (#12)

---
+++ components/Button.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 260px;
+  height: 60px;
+  font-size: 20px;
+  font-weight: bold;
+  font-style: italic;
+  background-color: #fff29c;
+  cursor: pointer;
+
+  img {
+    height: 1em;
+  }
+`;
+
+export default Button;
---
+++ components/Input.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  display: block;
+  margin: 0px auto;
+  width: 260px;
+  height: 60px;
+  font-size: 20px;
+  font-weight: bold;
+  font-style: italic;
+  margin-bottom: 20px;
+  padding: 10px;
+  background-color: #fff;
+`;
+
+export default Input;
